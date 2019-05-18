e095c02ce0d50fe6413923457375a61c034e506e
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-05-18 10:59:11 -0600

    set up db and created seeds.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,14 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS prestigeWW_db;
+CREATE DATABASE prestigeWW_db;
+
+USE prestigeWW_db;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
---
+++ models/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO `users` (`username`, `password`) VALUES
+                   (NULL, 'SUPER ADMIN', 'admin', 'admin'),
+                   ( 'NORMAL', 'robin', 'robin'),
+                   ( 'ADMIN', 'taylor', 'taylor'),
+                   ( 'ADMIN', 'vivian', 'vivian'),
+                   ( 'NORMAL', 'harry', 'harry'),
+                   ( 'ADMIN', 'melinda', 'melinda'),
+                   ( 'NORMAL', 'harley', 'harley');
