59151895775c7964b63223a966be03ff234515d0
aschaaf <schaaf1091@gmail.com>
2019-05-31 22:48:21 -0600

    fixed route authcontroller to authController

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-var authController = require("../controllers/authcontroller.js");
+var authController = require("../controllers/authController.js");
 var db = require("../models");
 
 module.exports = (app, passport) => {
